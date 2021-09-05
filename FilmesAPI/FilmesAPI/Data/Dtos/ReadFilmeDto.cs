d042bb076bd31712b1d8d9af21cec8584b98374d
Danilo Silva <danilo.silva@domvsit.com.br>
2021-09-04 23:28:41 -0300

    Mudando repositorio

---
+++ FilmesAPI/FilmesAPI/Data/Dtos/ReadFilmeDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class ReadFilmeDto
+    {
+        [Required(ErrorMessage = "O campo Titulo é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo Diretor é obrigatório")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O Gênero não pode passar de 30 caracteres")]
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A Duracao deve ter no mínimo 1 e no máximo 600 minutos")]
+        public int Duracao { get; set; }
+        public DateTime HoraDaConsulta { get; set; }    
+    }
+}
